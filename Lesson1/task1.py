32aba6d2bcc2870503799c1e7f3082fb6c6d5385
alex <skisk8fmx@gmail.com>
2022-02-13 13:13:25 +0300

    task 1

---
+++ Lesson1/task1.py
@@ -0,0 +1,35 @@
+"""
+Написать функцию host_ping(), в которой с помощью утилиты ping будет проверяться доступность сетевых узлов.
+Аргументом функции является список, в котором каждый сетевой узел должен быть представлен именем хоста или ip-адресом.
+В функции необходимо перебирать ip-адреса и проверять их доступность с выводом соответствующего сообщения
+ («Узел доступен», «Узел недоступен»). При этом ip-адрес сетевого узла должен создаваться с помощью функции
+ ip_address().
+"""
+import ipaddress
+import subprocess
+
+
+def host_ping(list_to_ping):
+    # словарь с результатами
+    ping_result = {"Узел доступен": [], "Узел недоступен": []}
+    for address in list_to_ping:
+        try:
+            # если получается, получаем ip и проверяем пинг
+            ping_process = subprocess.Popen(['ping', ipaddress.ip_address(address), '-c 1'], stdout=subprocess.PIPE)
+        except:
+            # если не получается, адресс введен не как ip и проверяем пинг
+            ping_process = subprocess.Popen(['ping', address, '-c 1'], stdout=subprocess.PIPE)
+        # ждем завершения пинга
+
+        ping_process.wait()
+        print(ping_process)
+        # добавляем в словарь результатов в зависимости от пинга
+        if ping_process.returncode == 0:
+            ping_result.get("Узел доступен").append(address)
+        else:
+            ping_result.get("Узел недоступен").append(address)
+    return ping_result
+
+
+if __name__ == '__main__':
+    print(host_ping(['yandex.ru', '156.485.477.11', 'google.ru', '192.168.0.1', 'net-takogo-adresa.ru']))
