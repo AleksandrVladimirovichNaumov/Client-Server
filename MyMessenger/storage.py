8e6692cf2059635e4aefb7f4410c7a9bcb0efd30
alex <skisk8fmx@gmail.com>
2022-02-22 16:05:14 +0300

    Messenger_storage - added contact table

--- MyMessenger/storage.py
+++ MyMessenger/storage.py
@@ -1,8 +1,8 @@
 import datetime
 
 from sqlalchemy import __version__, create_engine, Table, Column, MetaData, Integer, String, Boolean, DateTime, \
-    ForeignKey
-from sqlalchemy.orm import mapper, sessionmaker
+    ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.orm import mapper, sessionmaker, relationship
 
 
 class MessengerStorage:
@@ -37,6 +37,15 @@ class MessengerStorage:
         def __init__(self, user_id, datetime, ip, port):
             super().__init__(user_id, datetime, ip, port)
 
+    class ContactList():
+        """
+        класс для таблицы со cписком контактов
+        """
+
+        def __init__(self, owner_id, contact_id):
+            self.owner_id = owner_id
+            self.contact_id = contact_id
+
     def __init__(self):
         self.engine = create_engine('sqlite:///messenger_db.sqlite', echo=False)
         self.metadata = MetaData()
@@ -45,32 +54,41 @@ class MessengerStorage:
                                 Column('id', Integer, primary_key=True),
                                 Column('username', String),
                                 Column('last_login', DateTime))
-        # таблица со пользователями онлайн
+        # таблица с пользователями онлайн
         self.online_user_table = Table('Online_users', self.metadata,
                                        Column('id', Integer, primary_key=True),
                                        Column('user_id', ForeignKey('Users.id'), unique=True),
                                        Column('last_login', DateTime),
                                        Column('port', Integer),
                                        Column('ip', String))
-        # таблица со историями подключений
+        # таблица с историями подключений
         self.login_history_table = Table('Login_history', self.metadata,
                                          Column('id', Integer, primary_key=True),
                                          Column('user_id', ForeignKey('Users.id')),
                                          Column('last_login', DateTime),
                                          Column('port', Integer),
                                          Column('ip', String))
+
+        # таблица со списком контактов
+        self.contacts_table = Table('Contacts', self.metadata,
+                                    Column('id', Integer, primary_key=True),
+                                    Column('owner_id', ForeignKey('Users.id')),
+                                    Column('contact_id', ForeignKey('Users.id'))
+                                    )
         # создание таблиц
         self.metadata.create_all(self.engine)
         # связываем классы с таблицами
         mapper(self.AllUsers, self.user_table)
         mapper(self.OnlineUsers, self.online_user_table)
         mapper(self.LoginHistory, self.login_history_table)
+        mapper(self.ContactList, self.contacts_table)
         # создаем сессию
         server_session = sessionmaker(bind=self.engine)
         self.session = server_session()
         # обнуляем таблицу с активными юзерами при запуске сервера и сохраняем
         self.session.query(self.OnlineUsers).delete()
         self.session.commit()
+
     # геттеры
     def get_user_list(self):
         """
@@ -110,6 +128,21 @@ class MessengerStorage:
             self.LoginHistory.port
         ).join(self.AllUsers)
         return query.all()
+
+    def get_contacts_list(self, username):
+        """
+        геттер контактов
+        :return: список (username)
+        """
+        # определяем id владельца
+        owner_id = self.session.query(self.AllUsers).filter_by(username=username).first().id
+        # определяем id всех контактов
+        query = self.session.query(self.ContactList.contact_id).filter_by(owner_id=owner_id)
+        contact_list = []
+        # находим имя для каждого id
+        for i in query.all():
+            contact_list.append(self.session.query(self.AllUsers.username).filter_by(id=i[0]).first())
+        return contact_list
 
     def login(self, username, ip, port):
         """
@@ -153,6 +186,21 @@ class MessengerStorage:
         # сохраняем изменения
         self.session.commit()
 
+    def add_contact(self, owner_name, contact_name):
+        """
+        добавляем контакт
+        :param owner_name: владелец контакта
+        :param contact_name: имя контакта
+        :return: -
+        """
+        # находим id
+        owner_id = self.session.query(self.AllUsers).filter_by(username=owner_name).first().id
+        contact_id = self.session.query(self.AllUsers).filter_by(username=contact_name).first().id
+        # добавляем запись в таблицу
+        new_contact = self.ContactList(owner_id, contact_id)
+        self.session.add(new_contact)
+        self.session.commit()
+
 
 # Отладка
 if __name__ == '__main__':
@@ -171,3 +219,6 @@ if __name__ == '__main__':
     test_db.get_login_history_list()
     # выводим список известных пользователей
     print(test_db.get_user_list())
+
+    test_db.add_contact('client_1', 'client_2')
+    print(test_db.get_contacts_list('client_1'))
